db8077e8efcfed13ffbe7cb7828e24c51445d4a2
TharLinHtet <tharlin.htet@kbzbank.com>
2025-06-27 17:07:53 +0700

    ready to use - need refactor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Session } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import {
+  AuthenticationResponseJSON,
+  RegistrationResponseJSON,
+} from '@simplewebauthn/server';
 
 @Controller('auth')
 export class AuthController {
@@ -9,11 +13,75 @@ export class AuthController {
     return { status: 'ok' };
   }
   @Post('register/start')
-  async startRegistration(@Body() body: { username: string; email: string }) {
-    await this.authService.generateRegistrationOptions(
+  async startRegistration(
+    @Body() body: { username: string; email: string },
+    @Session() session: Record<string, any>,
+  ) {
+    const user = await this.authService.findOrCreateUserByEmail(
       body.username,
       body.email,
     );
-    return { message: 'Registration started' };
+    session.user = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const options = await this.authService.generateRegistrationOptions(user);
+    return options;
+  }
+
+  @Post('register')
+  async completeRegistration(
+    @Body()
+    body: {
+      email: string;
+      registrationResponse: RegistrationResponseJSON;
+    },
+  ) {
+    const user = await this.authService.findUserByEmail(body.email);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const verification = await this.authService.verifyRegistration(
+      body.registrationResponse,
+      user,
+    );
+    await this.authService.savePasskey(user, verification);
+
+    return { message: 'Registration completed' };
+  }
+
+  @Post('login/start')
+  async startLogin(@Body() body: { email: string }) {
+    const user = await this.authService.findUserByEmail(body.email);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const options = await this.authService.generateLoginOptions(user);
+    return options;
+  }
+
+  @Post('login')
+  async completeLogin(
+    @Body()
+    body: {
+      email: string;
+      loginResponse: AuthenticationResponseJSON;
+    },
+  ) {
+    const user = await this.authService.findUserByEmail(body.email);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const verification = await this.authService.verifyLogin(
+      body.loginResponse,
+      user,
+    );
+    return { message: 'Login completed', verification };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,43 +2,72 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDocument } from './schema/user.schema';
-import { generateRegistrationOptions } from '@simplewebauthn/server';
-import chalk from 'chalk';
+import {
+  AuthenticationResponseJSON,
+  generateAuthenticationOptions,
+  generateRegistrationOptions,
+  RegistrationResponseJSON,
+  VerifiedAuthenticationResponse,
+  VerifiedRegistrationResponse,
+  verifyAuthenticationResponse,
+  verifyRegistrationResponse,
+} from '@simplewebauthn/server';
+import { Passkey } from './schema/passkeys.schema';
 
 @Injectable()
 export class AuthService {
   private readonly rpName = 'WebAuthn Example';
-  private readonly rpID = 'localhost:5173';
-  private readonly origin = `https://${this.rpID}`;
+  private readonly rpID = 'localhost';
+  private readonly origin = `http://${this.rpID}:5173`;
 
   constructor(
     @InjectModel('User') private readonly userModel: Model<UserDocument>,
   ) {}
 
-  async findOrCreateUserByEmail(
-    username: string,
-    email: string,
-  ): Promise<UserDocument> {
+  async findUserByEmail(email: string): Promise<UserDocument | null> {
     const user = await this.userModel
       .findOne({ email })
       .populate('passkeys')
       .exec();
+
     if (!user) {
-      const newUser = new this.userModel({
-        email,
-        username,
-        currentChallenge: '',
-        passkeys: [],
-      });
-      await newUser.save();
-      return newUser;
+      return null;
+    }
+
+    return user;
+  }
+
+  async createNewUser(username: string, email: string): Promise<UserDocument> {
+    const newUser = new this.userModel({
+      username,
+      email,
+      passkeys: [],
+      currentChallenge: '',
+    });
+
+    try {
+      const savedUser = await newUser.save();
+
+      return savedUser;
+    } catch (error) {
+      throw new Error('Error creating new user' + error);
+    }
+  }
+
+  async findOrCreateUserByEmail(
+    username: string,
+    email: string,
+  ): Promise<UserDocument> {
+    const user = await this.findUserByEmail(email);
+    if (!user) {
+      return await this.createNewUser(username, email);
     }
     return user;
   }
 
-  async generateRegistrationOptions(username: string, email: string) {
-    const user = await this.findOrCreateUserByEmail(username, email);
-
+  async generateRegistrationOptions(
+    user: UserDocument,
+  ): Promise<PublicKeyCredentialCreationOptionsJSON> {
     const options: PublicKeyCredentialCreationOptionsJSON =
       await generateRegistrationOptions({
         rpName: this.rpName,
@@ -58,12 +87,7 @@ export class AuthService {
       });
 
     user.currentChallenge = options.challenge;
-    console.log(
-      chalk.bgCyan(
-        'Generated registration options for user:' + options.challenge,
-      ),
-      user.currentChallenge,
-    );
+    console.log(user.currentChallenge);
     await this.userModel.findOneAndUpdate(
       {
         _id: user._id,
@@ -74,5 +98,100 @@ export class AuthService {
     );
 
     return options;
+  }
+
+  async verifyRegistration(
+    body: RegistrationResponseJSON,
+    user: UserDocument,
+  ): Promise<VerifiedRegistrationResponse> {
+    const verification: VerifiedRegistrationResponse =
+      await verifyRegistrationResponse({
+        response: body,
+        expectedChallenge: user.currentChallenge,
+        expectedOrigin: this.origin,
+        expectedRPID: this.rpID,
+      });
+
+    const { verified } = verification;
+
+    if (!verified) {
+      throw new Error('Registration verification failed');
+    }
+    return verification;
+  }
+
+  async savePasskey(
+    user: UserDocument,
+    verification: VerifiedRegistrationResponse,
+  ): Promise<Passkey> {
+    const { registrationInfo } = verification;
+    const { credential } = registrationInfo!;
+
+    const passkey: Passkey = {
+      cred_id: credential.id,
+      cred_public_key: Buffer.from(credential.publicKey),
+      webauthn_user_id: user._id.toString(),
+      counter: credential.counter,
+      backup_eligible: false,
+      backup_status: false,
+      transports: credential.transports || [],
+      last_used: new Date(),
+      created_at: new Date(),
+    };
+
+    user.passkeys.push(passkey);
+    await user.save();
+
+    return passkey;
+  }
+
+  async generateLoginOptions(user: UserDocument) {
+    const options: PublicKeyCredentialRequestOptionsJSON =
+      await generateAuthenticationOptions({
+        rpID: this.rpID,
+        allowCredentials: user.passkeys.map((passkey) => ({
+          id: passkey.cred_id,
+          transports: passkey.transports,
+        })),
+      });
+
+    user.currentChallenge = options.challenge;
+
+    await this.userModel.findOneAndUpdate(
+      {
+        _id: user._id,
+      },
+      {
+        currentChallenge: options.challenge,
+      },
+    );
+
+    return options;
+  }
+
+  async verifyLogin(
+    body: AuthenticationResponseJSON,
+    user: UserDocument,
+  ): Promise<VerifiedAuthenticationResponse> {
+    const verification: VerifiedAuthenticationResponse =
+      await verifyAuthenticationResponse({
+        response: body,
+        expectedChallenge: user.currentChallenge,
+        expectedOrigin: this.origin,
+        expectedRPID: this.rpID,
+        credential: {
+          id: user.passkeys[0].cred_id,
+          publicKey: user.passkeys[0].cred_public_key,
+          counter: user.passkeys[0].counter,
+          transports: user.passkeys[0].transports,
+        },
+      });
+
+    const { verified } = verification;
+
+    if (!verified) {
+      throw new Error('Login verification failed');
+    }
+    return verification;
   }
 }
